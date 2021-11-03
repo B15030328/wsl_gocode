d17f03b3397a0a5b13b5f3af04df88c80f9a8308
chory <821466754@qq.com>
2021-11-03 10:34:37 +0800

    cookie代替session验证

--- imooc-product/fronted/middleware/auth.go
+++ imooc-product/fronted/middleware/auth.go
@@ -1,11 +1,23 @@
 package middleware
 
-import "github.com/kataras/iris/v12"
+import (
+	"fmt"
+	"imooc-product/encrypt"
+	"log"
+
+	"github.com/kataras/iris/v12"
+)
 
 func AuthConProduct(ctx iris.Context) {
 
-	uid := ctx.GetCookie("uid")
-	if uid == "" {
+	sign := ctx.GetCookie("sign")
+	fmt.Println(sign)
+	sign_decode, err := encrypt.DePwdCode(sign)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(sign_decode))
+	if string(sign_decode) == "" {
 		ctx.Application().Logger().Debug("必须先登录!")
 		ctx.Redirect("/user/login")
 		return
--- imooc-product/fronted/web/controllers/user_controller.go
+++ imooc-product/fronted/web/controllers/user_controller.go
@@ -1,33 +1,27 @@
 package controllers
 
 import (
+	"imooc-product/encrypt"
 	"imooc-product/models/define"
 	"imooc-product/models/mysql"
 	"imooc-product/services"
 	"imooc-product/tool"
+	"log"
 	"strconv"
-	"time"
 
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/mvc"
-	"github.com/kataras/iris/v12/sessions"
 )
 
 type UserController struct {
 	Ctx     iris.Context
 	Service services.UserService
-	Session *sessions.Sessions
 }
 
 // BeforeActivation 会被调用一次，在控制器适应主应用程序之前
 // 并且当然也是在服务运行之前
 func (u *UserController) BeforeActivation(b mvc.BeforeActivation) {
 	u.Service.Ins = *mysql.NewUserInstance()
-	sess := sessions.New(sessions.Config{
-		Cookie:  "AdminCookie",
-		Expires: 600 * time.Minute,
-	})
-	u.Session = sess
 }
 
 func (c *UserController) GetRegister() mvc.View {
@@ -80,8 +74,16 @@ func (c *UserController) PostLogin() mvc.Response {
 
 	//3、写入用户ID到cookie中
 	tool.GlobalCookie(c.Ctx, "uid", strconv.FormatInt(user.ID, 10))
-	session := c.Session.Start(c.Ctx)
-	session.Set("userID", strconv.FormatInt(user.ID, 10))
+	// 用cookie代替session 不需要再下面的写法
+	// session := c.Session.Start(c.Ctx)
+	// session.Set("userID", strconv.FormatInt(user.ID, 10))
+	uidByte := []byte(strconv.FormatInt(user.ID, 10))
+	uidString, err := encrypt.EnPwdCode(uidByte)
+	if err != nil {
+		log.Panic(err)
+	}
+	// 写入用户浏览器
+	tool.GlobalCookie(c.Ctx, "sign", uidString)
 	return mvc.Response{
 		Path: "/product/",
 	}
